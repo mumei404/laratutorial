0e1947396efad620d3484b7f517b934aa8cd00d7
shimazaki <lozld30@gmail.com>
2019-07-28 18:35:32 +0900

    t 24

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,13 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
-Route::get('contact', 'WelcomeController@contact');
-Route::get('about', 'PagesController@about');
-Route::get('articles', 'ArticlesController@index');
-Route::get('articles/create', 'ArticlesController@create');
-Route::get('articles/{id}', 'ArticlesController@show');
-Route::post('articles', 'ArticlesController@store');
-Route::get('articles/{id}/edit', 'ArticlesController@edit');
-Route::patch('articles/{id}', 'ArticlesController@update');
-Route::delete('articles/{id}', 'ArticlesController@destroy');
+Route::get('/', 'WelcomeController@index')->name('home');
+Route::get('contact', 'WelcomeController@contact')->name('contact');
+Route::get('about', 'PagesController@about')->name('about');
+Route::get('articles', 'ArticlesController@index')->name('articles.index');
+Route::get('articles/create', 'ArticlesController@create')->name('articles.create');
+Route::get('articles/{id}', 'ArticlesController@show')->name('articles.show');
+Route::post('articles', 'ArticlesController@store')->name('articles.store');
+Route::get('articles/{id}/edit', 'ArticlesController@edit')->name('articles.edit');
+Route::patch('articles/{id}', 'ArticlesController@update')->name('articles.update');
+Route::delete('articles/{id}', 'ArticlesController@destroy')->name('articles.destroy');
