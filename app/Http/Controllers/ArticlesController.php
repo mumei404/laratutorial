e45472a0e70b8a7148a4a4e4f7911f0265d12c77
shimazaki <lozld30@gmail.com>
2019-07-28 17:48:44 +0900

    t20

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Article;
 use App\Http\Requests\ArticleRequest;
+use Carbon\Carbon;
 
 class ArticlesController extends Controller
 {
 	public function index()
 	{
-		$articles = Article::all();
+		$articles = Article::latest('published_at')->latest('created_at')
+			->published()
+			->get();
 
 		return view('articles.index', compact('articles'));
 	}
